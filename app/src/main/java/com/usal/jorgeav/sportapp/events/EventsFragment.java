ae8fd7b7b4fd715305729b951842d0e1bc8cd0da
jorgeavilae <jorgeavilae@gmail.com>
2017-04-25 21:54:31 +0200

    Add EventsFragment

---
+++ app/src/main/java/com/usal/jorgeav/sportapp/events/EventsFragment.java
@@ -0,0 +1,65 @@
+package com.usal.jorgeav.sportapp.events;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.usal.jorgeav.sportapp.R;
+import com.usal.jorgeav.sportapp.data.Event;
+import com.usal.jorgeav.sportapp.data.EventsRepository;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EventsFragment extends Fragment implements EventsContract.View  {
+
+    EventsContract.Presenter mEventsPresenter;
+    EventsAdapter mEventsRecyclerAdapter;
+
+    @BindView(R.id.events_list)
+    RecyclerView eventsRecyclerList;
+
+    public EventsFragment() {
+        // Required empty public constructor
+    }
+
+    public static EventsFragment newInstance() {
+        return new EventsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mEventsPresenter = new EventsPresenter(new EventsRepository(), this);
+        mEventsRecyclerAdapter = new EventsAdapter(null);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_events, container, false);
+        ButterKnife.bind(this, root);
+
+        eventsRecyclerList.setAdapter(mEventsRecyclerAdapter);
+        eventsRecyclerList.setHasFixedSize(true);
+        eventsRecyclerList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mEventsPresenter.loadEvents();
+    }
+
+    @Override
+    public void showEvents(List<Event> events) {
+        mEventsRecyclerAdapter.replaceData(events);
+    }
+}
