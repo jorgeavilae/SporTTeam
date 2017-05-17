7f244cc4d834028ec0ae67f739003f61d2bcc735
jorgeavilae <jorgeavilae@gmail.com>
2017-05-17 18:54:29 +0200

    Completed SportteamDBHelper

--- app/src/main/java/com/usal/jorgeav/sportapp/data/provider/SportteamDBHelper.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/data/provider/SportteamDBHelper.java
@@ -27,11 +27,52 @@ public class SportteamDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_USER_TABLE = "CREATE TABLE " + SportteamContract.TABLE_USER + " (" +
+                SportteamContract.UserEntry._ID         + " INTEGER PRIMARY KEY,"       +
+                SportteamContract.UserEntry.USER_ID     + " TEXT UNIQUE NOT NULL,"      +
+                SportteamContract.UserEntry.EMAIL       + " TEXT UNIQUE NOT NULL,"      +
+                SportteamContract.UserEntry.NAME        + " TEXT UNIQUE NOT NULL,"      +
+                SportteamContract.UserEntry.AGE         + " INTEGER,"                   +
+                SportteamContract.UserEntry.CITY        + " TEXT,"                      +
+                SportteamContract.UserEntry.PHOTO       + " TEXT,"                      +
+                " UNIQUE (" + SportteamContract.UserEntry.USER_ID + ") ON CONFLICT REPLACE);";
 
+        final String SQL_CREATE_EVENT_TABLE = "CREATE TABLE " + SportteamContract.TABLE_EVENT + " (" +
+                SportteamContract.EventEntry._ID                + " INTEGER PRIMARY KEY,"       +
+                SportteamContract.EventEntry.EVENT_ID           + " TEXT UNIQUE NOT NULL,"      +
+                SportteamContract.EventEntry.SPORT              + " TEXT NOT NULL,"             +
+                SportteamContract.EventEntry.FIELD              + " TEXT,"                      +
+                SportteamContract.EventEntry.CITY               + " TEXT,"                      +
+                SportteamContract.EventEntry.DATE               + " INTEGER,"                   +
+                SportteamContract.EventEntry.OWNER              + " TEXT NOT NULL,"             +
+                SportteamContract.EventEntry.TOTAL_PLAYERS      + " INTEGER,"                   +
+                SportteamContract.EventEntry.EMPTY_PLAYERS      + " INTEGER,"                   +
+                " UNIQUE (" + SportteamContract.EventEntry.EVENT_ID + ") ON CONFLICT REPLACE);";
+
+        final String SQL_CREATE_FIELD_TABLE = "CREATE TABLE " + SportteamContract.TABLE_FIELD + " (" +
+                SportteamContract.FieldEntry._ID                + " INTEGER PRIMARY KEY,"       +
+                SportteamContract.FieldEntry.FIELD_ID           + " TEXT NOT NULL,"      +
+                SportteamContract.FieldEntry.NAME               + " TEXT NOT NULL,"      +
+                SportteamContract.FieldEntry.SPORT              + " TEXT NOT NULL,"             +
+                SportteamContract.FieldEntry.ADDRRESS           + " TEXT,"                      +
+                SportteamContract.FieldEntry.CITY               + " TEXT,"                      +
+                SportteamContract.FieldEntry.PUNTUATION         + " REAL,"                      +
+                SportteamContract.FieldEntry.VOTES              + " INTEGER,"                   +
+                SportteamContract.FieldEntry.OPENING_TIME       + " INTEGER,"                   +
+                SportteamContract.FieldEntry.CLOSING_TIME       + " INTEGER,"                   +
+                " UNIQUE (" + SportteamContract.FieldEntry.FIELD_ID + ", "
+                            + SportteamContract.FieldEntry.SPORT    + ") ON CONFLICT REPLACE);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_USER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_EVENT_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_FIELD_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + SportteamContract.TABLE_USER);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + SportteamContract.TABLE_EVENT);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + SportteamContract.TABLE_FIELD);
+        onCreate(sqLiteDatabase);
     }
 }
