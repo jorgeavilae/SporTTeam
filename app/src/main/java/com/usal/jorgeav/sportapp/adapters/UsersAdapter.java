b0dbac87aec24fc28c7eb0736123be9941964966
jorgeavilae <jorgeavilae@gmail.com>
2017-05-26 14:18:47 +0200

    Add otherUser from Adapter to item_list logic

--- app/src/main/java/com/usal/jorgeav/sportapp/adapters/UsersAdapter.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/adapters/UsersAdapter.java
@@ -2,12 +2,19 @@ package com.usal.jorgeav.sportapp.adapters;
 
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
+import com.usal.jorgeav.sportapp.R;
 import com.usal.jorgeav.sportapp.data.User;
+import com.usal.jorgeav.sportapp.data.provider.SportteamContract;
 
+import java.util.Locale;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -25,12 +32,23 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> 
 
     @Override
     public UsersAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.user_item_list, parent, false);
+
+        return new UsersAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(UsersAdapter.ViewHolder holder, int position) {
-
+        if (mDataset.moveToPosition(position)) {
+            int age = mDataset.getInt(SportteamContract.UserEntry.COLUMN_AGE);
+            holder.textViewUserId.setText(mDataset.getString(SportteamContract.UserEntry.COLUMN_USER_ID));
+            holder.textViewUserEmail.setText(mDataset.getString(SportteamContract.UserEntry.COLUMN_EMAIL));
+            holder.textViewUserName.setText(mDataset.getString(SportteamContract.UserEntry.COLUMN_NAME));
+            holder.textViewUserAge.setText(String.format(Locale.getDefault(), "%2d", age));
+            holder.textViewUserCity.setText(mDataset.getString(SportteamContract.UserEntry.COLUMN_CITY));
+            holder.textViewUserPhoto.setText(mDataset.getString(SportteamContract.UserEntry.COLUMN_PHOTO));
+        }
     }
 
     public void replaceData(Cursor users) {
@@ -48,6 +66,18 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements AdapterView.OnClickListener {
+        @BindView(R.id.user_item_id)
+        TextView textViewUserId;
+        @BindView(R.id.user_item_email)
+        TextView textViewUserEmail;
+        @BindView(R.id.user_item_name)
+        TextView textViewUserName;
+        @BindView(R.id.user_item_age)
+        TextView textViewUserAge;
+        @BindView(R.id.user_item_city)
+        TextView textViewUserCity;
+        @BindView(R.id.user_item_photo)
+        TextView textViewUserPhoto;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -57,6 +87,8 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> 
 
         @Override
         public void onClick(View view) {
+            //TODO manage this properly
+            mClickListener.onUserClick(null);
         }
     }
 
