3139aa35225b1d22c0dc83223a95ee7a70ada837
jorgeavilae <jorgeavilae@gmail.com>
2017-07-12 22:11:38 +0200

    Simulate Participant for Events: Moved Firebase code to FirebaseActions.java

--- app/src/main/java/com/usal/jorgeav/sportapp/eventdetail/simulateparticipant/SimulateParticipantPresenter.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/eventdetail/simulateparticipant/SimulateParticipantPresenter.java
@@ -1,17 +1,13 @@
 package com.usal.jorgeav.sportapp.eventdetail.simulateparticipant;
 
 import android.net.Uri;
-import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
-import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.usal.jorgeav.sportapp.network.firebase.FirebaseDBContract;
-import com.usal.jorgeav.sportapp.utils.Utiles;
+import com.usal.jorgeav.sportapp.mainactivities.BaseActivity;
+import com.usal.jorgeav.sportapp.network.firebase.FirebaseActions;
 
 /**
  * Created by Jorge Avila on 12/07/2017.
@@ -22,53 +18,33 @@ public class SimulateParticipantPresenter implements SimulateParticipantContract
     SimulateParticipantContract.View mView;
     String mEventId;
     String mName;
-    String mAge;
+    int mAge;
 
     public SimulateParticipantPresenter(SimulateParticipantContract.View view){
         this.mView = view;
-        String mEventId = "";
-        String mName = "";
-        String mAge = "";
+        mEventId = "";
+        mName = "";
+        mAge = -1;
     }
 
     @Override
     public void addSimulatedParticipant(String eventId, String name, Uri photo, String age) {
-        if(true) {//Validate arguments
+        if(eventId != null && !TextUtils.isEmpty(eventId) && TextUtils.isEmpty(name)
+                && !TextUtils.isEmpty(age)) { //Validate arguments
             mEventId = eventId;
             mName = name;
-            mAge = age;
+            mAge = Integer.parseInt(age);
             if (photo != null)
                 storePhotoOnFirebase(photo);
             else {
                 storeSimulatedParticipantInFirebase(null);
             }
         }
-
-//        ((BaseActivity)mView.getActivityContext()).onBackPressed();
+        ((BaseActivity)mView.getActivityContext()).onBackPressed();
     }
 
     private void storePhotoOnFirebase(Uri photo) {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference photoRef = storage.getReferenceFromUrl(Utiles.getFirebaseStorageRootReference())
-                .child(FirebaseDBContract.Storage.PROFILE_PICTURES).child(photo.getLastPathSegment());
-
-        // Upload file to Firebase Storage
-        // Create the file metadata
-        StorageMetadata metadata = new StorageMetadata.Builder()
-                .setContentType("image/jpg")
-                .build();
-
-        // Upload file and metadata to the path
-        UploadTask uploadTask = photoRef.putFile(photo, metadata);
-
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle unsuccessful uploads
-                Log.e(TAG, "storePhotoOnFirebase:putFile:onFailure: ", exception);
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        FirebaseActions.storePhotoOnFirebase(photo, new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 // Handle successful uploads on complete
@@ -79,6 +55,8 @@ public class SimulateParticipantPresenter implements SimulateParticipantContract
     }
 
     private void storeSimulatedParticipantInFirebase(Uri photoUriInFirebase) {
+
+        FirebaseActions.addSimulatedParticipant(mEventId, mName, photoUriInFirebase, mAge);
         Log.d(TAG, "addSimulatedParticipant: eventId "+mEventId);
         Log.d(TAG, "addSimulatedParticipant: name "+mName);
         Log.d(TAG, "addSimulatedParticipant: photo "+photoUriInFirebase);
