741d867cc024fe1fdc01dfa3148bfa39ed48b45b
jorgeavilae <jorgeavilae@gmail.com>
2017-07-25 20:02:03 +0200

    Add Field's marker layout for MapActivity

--- app/src/main/java/com/usal/jorgeav/sportapp/adapters/MapMarkerInfoAdapter.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/adapters/MapMarkerInfoAdapter.java
@@ -2,13 +2,15 @@ package com.usal.jorgeav.sportapp.adapters;
 
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.usal.jorgeav.sportapp.MyApplication;
 import com.usal.jorgeav.sportapp.R;
 import com.usal.jorgeav.sportapp.data.Field;
-import com.usal.jorgeav.sportapp.utils.UtilesTime;
+import com.usal.jorgeav.sportapp.data.SportCourt;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +25,7 @@ public class MapMarkerInfoAdapter implements GoogleMap.InfoWindowAdapter {
     private List<Field> mDataset;
 
     public MapMarkerInfoAdapter(LayoutInflater layoutInflater, ArrayList<Field> fieldList) {
-        //TODO make new marker layout
-        this.mContent = layoutInflater.inflate(R.layout.fields_item_list, null);
+        this.mContent = layoutInflater.inflate(R.layout.field_marker, null);
         this.mDataset = fieldList;
     }
 
@@ -44,20 +45,36 @@ public class MapMarkerInfoAdapter implements GoogleMap.InfoWindowAdapter {
     }
 
     private void populate(View view, Field field) {
-        TextView textViewFieldId = (TextView) view.findViewById(R.id.fields_item_id);
-        textViewFieldId.setText(field.getId());
-
-        TextView textViewFieldName = (TextView) view.findViewById(R.id.fields_item_name);
+        TextView textViewFieldName = (TextView) view.findViewById(R.id.field_marker_name);
         textViewFieldName.setText(field.getName());
 
-        TextView textViewFieldAddress = (TextView) view.findViewById(R.id.fields_item_address);
+        TextView textViewFieldAddress = (TextView) view.findViewById(R.id.field_marker_address);
         textViewFieldAddress.setText(field.getAddress());
 
-        TextView textViewFieldOpening = (TextView) view.findViewById(R.id.fields_item_opening);
-        textViewFieldOpening.setText(UtilesTime.millisToTimeString(field.getOpening_time()));
+        TextView textViewSportMore = (TextView) view.findViewById(R.id.field_marker_sport_more);
+        textViewSportMore.setVisibility(View.GONE);
+        ImageView imageViewFieldSport1 = (ImageView) view.findViewById(R.id.field_marker_sport1);
+        imageViewFieldSport1.setVisibility(View.GONE);
+        ImageView imageViewFieldSport2 = (ImageView) view.findViewById(R.id.field_marker_sport2);
+        imageViewFieldSport1.setVisibility(View.GONE);
 
-        TextView textViewFieldClosing = (TextView) view.findViewById(R.id.fields_item_closing);
-        textViewFieldClosing.setText(UtilesTime.millisToTimeString(field.getClosing_time()));
+        if (field.getSport() != null && field.getSport().size() > 0) {
+            ArrayList<SportCourt> sports = new ArrayList<>(field.getSport().values());
+
+            int sport1Icon = MyApplication.getAppContext().getResources().getIdentifier(
+                    sports.get(0).getSport_id() , "drawable", MyApplication.getAppContext().getPackageName());
+            imageViewFieldSport1.setImageResource(sport1Icon);
+            imageViewFieldSport1.setVisibility(View.VISIBLE);
+
+            if (sports.size() > 1) {
+                int sport2Icon = MyApplication.getAppContext().getResources().getIdentifier(
+                        sports.get(1).getSport_id() , "drawable", MyApplication.getAppContext().getPackageName());
+                imageViewFieldSport2.setImageResource(sport2Icon);
+                imageViewFieldSport2.setVisibility(View.VISIBLE);
+            }
+
+            if (sports.size() > 2) textViewSportMore.setVisibility(View.VISIBLE);
+        }
 
     }
 }
