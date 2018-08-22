f6ecb0277718fdb6f272213e1ed7aa70e9cc697d
jorgeavilae <jorgeavilae@gmail.com>
2018-08-22 20:57:59 +0200

    Javadoc package ..

--- app/src/main/java/com/usal/jorgeav/sportapp/BaseFragment.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/BaseFragment.java
@@ -8,13 +8,34 @@ import android.view.inputmethod.InputMethodManager;
 
 import com.usal.jorgeav.sportapp.mainactivities.ActivityContracts;
 
+/**
+ * Representa los Fragmentos de la aplicacion. Ha sido creada para implementar el codigo
+ * comun al resto de Fragmentos, para que estos obtengan esa funcionalidad mediante herencia.
+ */
 public abstract class BaseFragment extends Fragment {
+    /**
+     * Nombre de la clase
+     */
     @SuppressWarnings("unused")
     private static final String TAG = BaseFragment.class.getSimpleName();
 
+    /**
+     * Referencia a la interfaz {@link ActivityContracts.FragmentManagement} posiblemente
+     * implementada por Actividad contenedora del Fragmento
+     */
     protected ActivityContracts.FragmentManagement mFragmentManagementListener;
+    /**
+     * Referencia a la interfaz {@link ActivityContracts.NavigationDrawerManagement} posiblemente
+     * implementada por Actividad contenedora del Fragmento
+     */
     protected ActivityContracts.NavigationDrawerManagement mNavigationDrawerManagementListener;
 
+    /**
+     * En este metodo del ciclo de vida se asocian las varibales a la Actividad, en el caso de que
+     * haya implementado las interfaces de {@link ActivityContracts}
+     *
+     * @param context {@link Context} de Actividad
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -24,6 +45,9 @@ public abstract class BaseFragment extends Fragment {
             mNavigationDrawerManagementListener = (ActivityContracts.NavigationDrawerManagement) context;
     }
 
+    /**
+     * Proceso inverso a {@link #onAttach(Context)}: anula las variables de las interfaces
+     */
     @Override
     public void onDetach() {
         super.onDetach();
@@ -32,34 +56,61 @@ public abstract class BaseFragment extends Fragment {
         mNavigationDrawerManagementListener = null;
     }
 
+    /**
+     *
+     * @return {@link Context} de la Actividad contedora
+     */
     public Context getActivityContext() {
         return getActivity();
     }
 
+    /**
+     *
+     * @return {@link BaseFragment} que referencia a este Fragmento
+     */
     public BaseFragment getThis() { return this;}
 
+    /**
+     * Utiliza {@link #mFragmentManagementListener} para mostrar el Fragmento
+     */
     public void showContent() {
         if (mFragmentManagementListener != null)
             mFragmentManagementListener.showContent();
     }
 
+    /**
+     * Utiliza {@link #mFragmentManagementListener} para ocultar el Fragmento
+     */
     public void hideContent() {
         if (mFragmentManagementListener != null)
             mFragmentManagementListener.hideContent();
     }
 
+    /**
+     * Obtiene una referencia a {@link FragmentManager} y la usa para limpiar
+     * la pila de Fragmentos
+     */
     public void resetBackStack() {
         getActivity().getSupportFragmentManager().popBackStack(
                 getActivity().getSupportFragmentManager().getBackStackEntryAt(0).getId(),
                 FragmentManager.POP_BACK_STACK_INCLUSIVE);
     }
 
-    /* https://stackoverflow.com/a/1109108/4235666 */
+    /**
+     * Obtiene una referencia a la {@link View} que tiene el foco de la interfaz y, si esta
+     * mostrando el teclado flotante en la pantalla, lo esconde
+     *
+     * {@link
+     * <a href= "https://stackoverflow.com/a/17789187/4235666">
+     *     (StackOverflow) Close/hide the Android Soft Keyboard
+     * </a>}
+     */
     public void hideSoftKeyboard() {
         View view = getActivity().getCurrentFocus();
         if (view != null) {
             InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            if (imm != null)
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
 }
--- app/src/main/java/com/usal/jorgeav/sportapp/MyApplication.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/MyApplication.java
@@ -3,14 +3,30 @@ package com.usal.jorgeav.sportapp;
 import android.app.Application;
 import android.content.Context;
 
+/**
+ * Clase heredera de {@link Application} que representa toda la aplicacion.
+ * <p>
+ * Usada para guardar una referencia estatica al {@link Context} de Aplicacion
+ */
 public class MyApplication extends Application {
+    /**
+     * Referncia estatica a {@link Context} de Aplicacion
+     */
     private static Context context;
 
+    /**
+     * Invocado cuando la aplicacion esta iniciandose, guarda la referencia a Context
+     */
+    @Override
     public void onCreate() {
         super.onCreate();
         MyApplication.context = getApplicationContext();
     }
 
+    /**
+     * Metodo de acceso a la variable estatica {@link Context} de Aplicacion
+     * @return {@link MyApplication#context}
+     */
     public static Context getAppContext() {
         return MyApplication.context;
     }
