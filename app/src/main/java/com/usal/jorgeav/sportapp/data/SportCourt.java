bcd49fa5110170db4bf6e8c8d0db9b7643f98677
jorgeavilae <jorgeavilae@gmail.com>
2018-08-23 21:22:05 +0200

    Javadoc package data

--- app/src/main/java/com/usal/jorgeav/sportapp/data/SportCourt.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/data/SportCourt.java
@@ -1,5 +1,6 @@
 package com.usal.jorgeav.sportapp.data;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -10,27 +11,52 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 /**
- * Parcelable
- * se guarda en lista
- * {@link com.usal.jorgeav.sportapp.mainactivities.FieldsActivity#onSaveInstanceState(Bundle)}
- * se recupera en lista
- * {@link com.usal.jorgeav.sportapp.mainactivities.FieldsActivity#onRestoreInstanceState(Bundle)}
- * si necesita q se mantenga para la creacion de nueva instalc, primero introducir deportes - segundo introducir datos - tercero envia a firebase
- * no necesita q se mantenga para la creacion de nuevo usuario, primero introducir datos - segundo introducir deportes - tercero envia a firebase
- * si durante el segundo paso se cambia de app y se vuelve, los deportes se perderian en la creacion de instalacion si no se guardan en parcelable
+ * Representa una pista de una instalación
+ * <p></p>
+ * Implementa la interfaz Parcelable para poder guardar este {@link Object} en variables de
+ * estado entre cambios de configuración
+ * ({@link android.app.Activity#onConfigurationChanged(Configuration)}, o en {@link android.content.Intent}
+ * para enviarla a otra {@link android.app.Activity}
+ * <p></p>
+ * Implementa la interfaz Serializable para poder guardar este {@link Object} en Parcel
+ * de {@link Field} en el método {@link Field#writeToParcel(Parcel, int)}
  *
- * Serializable
- * {@link Field#writeToParcel(Parcel, int)} en {@link Parcel#writeSerializable(Serializable)}
+ * @see com.usal.jorgeav.sportapp.mainactivities.FieldsActivity#onSaveInstanceState(Bundle)
+ * @see com.usal.jorgeav.sportapp.mainactivities.FieldsActivity#onRestoreInstanceState(Bundle)
  */
 public class SportCourt implements Parcelable, Serializable {
+    /**
+     * Identificador del deporte de la pista
+     */
     private String sport_id;
+    /**
+     * Puntuación de la pista
+     */
     private Double punctuation;
+    /**
+     * Cantidad de votos emitidos para la pista
+     */
     private Long votes;
 
+    /**
+     * Constructor sin argumentos. Necesario para el parseo de este objeto desde Firebase
+     * Realtime Database con
+     * {@link
+     * <a href= "https://firebase.google.com/docs/reference/admin/java/reference/com/google/firebase/database/DataSnapshot.html#getValue(com.google.firebase.database.GenericTypeIndicator%3CT%3E)">
+     *     DataSnapshot.getValue(Class)
+     * </a>}
+     */
     public SportCourt() {
         // Default constructor required for calls to DataSnapshot.getValue(SportCourt.class)
     }
 
+    /**
+     * Constructor con argumentos
+     *
+     * @param sport_id identificador de deporte
+     * @param punctuation puntuacion
+     * @param votes número de votos
+     */
     public SportCourt(String sport_id, Double punctuation, Long votes) {
         this.sport_id = sport_id;
         this.punctuation = punctuation;
@@ -57,6 +83,26 @@ public class SportCourt implements Parcelable, Serializable {
         this.votes = votes;
     }
 
+    /**
+     * Introduce los datos de este objeto en un Map para Firebase Realtime Database.
+     * La clave se obtiene de la clase diccionario {@link FirebaseDBContract} y como valor se
+     * utilizan las variables de la clase.
+     *
+     * @return Map con los datos de {@link SportCourt}
+     */
+    public Object toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put(FirebaseDBContract.SportCourt.SPORT_ID, this.sport_id);
+        result.put(FirebaseDBContract.SportCourt.PUNCTUATION, this.punctuation);
+        result.put(FirebaseDBContract.SportCourt.VOTES, this.votes);
+        return result;
+    }
+
+    /**
+     * Representación del objeto en cadena de texto
+     *
+     * @return la cadena de texto con los datos del objeto
+     */
     @Override
     public String toString() {
         return "SportCourt{" +
@@ -66,19 +112,22 @@ public class SportCourt implements Parcelable, Serializable {
                 '}';
     }
 
-    public Object toMap() {
-        HashMap<String, Object> result = new HashMap<>();
-        result.put(FirebaseDBContract.SportCourt.SPORT_ID, this.sport_id);
-        result.put(FirebaseDBContract.SportCourt.PUNCTUATION, this.punctuation);
-        result.put(FirebaseDBContract.SportCourt.VOTES, this.votes);
-        return result;
-    }
-
+    /**
+     * Describe los contenidos del objeto mediante un entero que representa una máscara de bits
+     *
+     * @return 0 (máscara de bits)
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Escribe este {@link SportCourt} en un {@link Parcel}
+     *
+     * @param dest Destino de la operación
+     * @param flags opcional
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.sport_id);
@@ -86,12 +135,21 @@ public class SportCourt implements Parcelable, Serializable {
         dest.writeValue(this.votes);
     }
 
+    /**
+     * Constructor que ejecuta una operación inversa a {@link #writeToParcel(Parcel, int)}
+     *
+     * @param in Parcel del que extraer los datos para {@link SportCourt}
+     */
     protected SportCourt(Parcel in) {
         this.sport_id = in.readString();
         this.punctuation = (Double) in.readValue(Double.class.getClassLoader());
         this.votes = (Long) in.readValue(Long.class.getClassLoader());
     }
 
+    /**
+     * Variable estática usada para crear instancias de {@link SportCourt} a partir del
+     * {@link Parcel} creado en {@link #writeToParcel(Parcel, int)}
+     */
     public static final Parcelable.Creator<SportCourt> CREATOR = new Parcelable.Creator<SportCourt>() {
         @Override
         public SportCourt createFromParcel(Parcel source) {
