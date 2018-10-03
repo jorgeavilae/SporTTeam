7645316597a4e5329e4e109eaea5aa0485b8b834
jorgeavilae <jorgeavilae@gmail.com>
2018-10-03 11:50:42 +0200

    Javadoc spoortselection

--- app/src/main/java/com/usal/jorgeav/sportapp/sportselection/SelectSportFragment.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/sportselection/SelectSportFragment.java
@@ -23,33 +23,91 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SelectSportFragment extends BaseFragment implements SelectSportsAdapter.OnSelectSportClickListener {
+/**
+ * Fragmento utilizado para mostrar los deportes admitidos en la aplicación con el fin de que el
+ * usuario seleccione uno. Se encarga de inicializar los componentes de la interfaz para mostrar la
+ * lista de deportes cargados desde el archivo de recursos correspondiente (arrays.xml). Implementa
+ * la interfaz {@link SelectSportsAdapter.OnSelectSportClickListener} para manejar la
+ * pulsación sobre uno de los deportes de la colección. También declara la interfaz
+ * {@link OnSportSelectedListener} para ser implementada por la Actividad contenedora a la que
+ * comunicar el deporte seleccionado.
+ */
+public class SelectSportFragment extends BaseFragment implements
+        SelectSportsAdapter.OnSelectSportClickListener {
+    /**
+     * Nombre de la clase
+     */
     private final static String TAG = SelectSportFragment.class.getSimpleName();
 
+    /**
+     * Referencia a la lista de la interfaz donde se van a colocar los deportes
+     */
     @BindView(R.id.recycler_list)
     RecyclerView sportsRecyclerViewList;
+
+    /**
+     * Adaptador para manejar la colección de deportes y mostrar sus datos en cada una de las
+     * celdas de la lista.
+     */
     private SelectSportsAdapter mSportAdapter;
 
+    /**
+     * Constructor sin argumentos.
+     */
     public SelectSportFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Método de instanciación del Fragmento
+     *
+     * @return una nueva instancia de SelectSportFragment
+     */
     public static SelectSportFragment newInstance() {
         return new SelectSportFragment();
     }
 
+    /**
+     * Instancia el Adaptador usado en la lista de deportes. Este Fragmento no inicializa el
+     * Presentador porque no se rige bajo el patrón MVP.
+     *
+     * @param savedInstanceState estado del Fragmento guardado en una posible rotación de
+     *                           la pantalla, o null.
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        mSportAdapter = new SelectSportsAdapter(null, this, Glide.with(this));
     }
 
+    /**
+     * Inicializa el contenido del menú de opciones de la esquina superior derecha de la pantalla:
+     * lo limpia para no mostrar ninguna opción.
+     *
+     * @param menu menú de opciones donde se van a emplazar los elementos.
+     */
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         menu.clear();
     }
 
+    /**
+     * Inicializa y obtiene una referencia a los elementos de la interfaz con la ayuda de
+     * ButterKnife. Establece {@link #mSportAdapter} como adaptador de la lista de la interfaz
+     * recién inflada.
+     * <p>
+     * Además, comprueba que la Actividad contenedora implementa la interfaz
+     * {@link OnSportSelectedListener} para comunicar el resultado de la ejecución de este Fragmento.
+     *
+     * @param inflater           utilizado para inflar el archivo de layout
+     * @param container          contenedor donde se va a incluir la interfaz o null
+     * @param savedInstanceState estado del Fragmento guardado en una posible rotación de
+     *                           la pantalla, o null.
+     * @return la vista de la interfaz inicializada
+     * @see <a href= "http://jakewharton.github.io/butterknife/">ButterKnife</a>
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -60,9 +118,6 @@ public class SelectSportFragment extends BaseFragment implements SelectSportsAda
             Log.e(TAG, "onCreateView: getActivity() should implement OnSportSelectedListener");
             getActivity().onBackPressed();
         }
-        mSportAdapter = new SelectSportsAdapter(null,
-                this,
-                Glide.with(this));
         sportsRecyclerViewList.setAdapter(mSportAdapter);
         sportsRecyclerViewList.setHasFixedSize(true);
         sportsRecyclerViewList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
@@ -70,6 +125,13 @@ public class SelectSportFragment extends BaseFragment implements SelectSportsAda
         return root;
     }
 
+    /**
+     * Al finalizar el proceso de creación de la Actividad contenedora, se invoca este método que
+     * establece un título para la barra superior y la acción que debe realizar: navegar hacia atrás.
+     *
+     * @param savedInstanceState estado del Fragmento guardado en una posible rotación de
+     *                           la pantalla, o null.
+     */
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -77,6 +139,25 @@ public class SelectSportFragment extends BaseFragment implements SelectSportsAda
         mNavigationDrawerManagementListener.setToolbarAsUp();
     }
 
+    /**
+     * Establece los deportes que debe mostrar la lista en el Adaptador, por medio de
+     * {@link #loadSports()}
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        hideSoftKeyboard();
+        mSportAdapter.replaceData(loadSports());
+        showContent();
+    }
+
+    /**
+     * Carga los identificadores de deportes desde el array declarado en el archivo de recursos
+     * arrays.xml, y crea para cada uno un {@link Sport} que será añadido al Adaptador de la lista.
+     *
+     * @return lista de {@link Sport}s que serán añadidos al Adaptador de la lista para que los
+     * muestre en la interfaz
+     */
     private List<Sport> loadSports() {
         ArrayList<Sport> result = new ArrayList<>();
 
@@ -88,29 +169,37 @@ public class SelectSportFragment extends BaseFragment implements SelectSportsAda
         return result;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        hideSoftKeyboard();
-//        mFragmentManagementListener.setCurrentDisplayedFragment(getString(R.string.pick_sport), this);
-//        if (mNavigationDrawerManagementListener != null) mNavigationDrawerManagementListener.setToolbarAsUp();
-        mSportAdapter.replaceData(loadSports());
-        showContent();
-    }
-
+    /**
+     * Borra el contenido del Adaptador para que no se guarde en el estado del Fragmento.
+     */
     @Override
     public void onPause() {
         super.onPause();
         mSportAdapter.replaceData(null);
     }
 
+    /**
+     * Invocado al pulsar sobre un deporte de la lista, envía el identificador del deporte
+     * seleccionado a la Actividad contenedora por medio de {@link OnSportSelectedListener}
+     *
+     * @param sportId Identificador del Deporte seleccionado
+     */
     @Override
     public void onSportClick(String sportId) {
         if (getActivity() instanceof OnSportSelectedListener)
             ((OnSportSelectedListener) getActivity()).onSportSelected(sportId);
     }
 
+    /**
+     * Interfaz que debe ser implementada por la Actividad que contenga este Fragmento
+     * {@link SelectSportFragment} para recibir el identificador del deporte seleccionado.
+     */
     public interface OnSportSelectedListener {
+        /**
+         * Invocado para comunicar el deporte seleccionado
+         *
+         * @param sportId identificador del deporte seleccionado
+         */
         void onSportSelected(String sportId);
     }
 }
--- app/src/main/java/com/usal/jorgeav/sportapp/sportselection/SportsListFragment.java
+++ app/src/main/java/com/usal/jorgeav/sportapp/sportselection/SportsListFragment.java
@@ -27,21 +27,64 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-
+/**
+ * Fragmento utilizado para mostrar la colección de deportes de la aplicación junto con una barra de
+ * puntuación con el fin de especificar un nivel o una puntuación para cada uno. Se aplica a los
+ * deportes practicados por un usuario {@link Sport} o a las pistas de una instalación
+ * {@link com.usal.jorgeav.sportapp.data.SportCourt}. Se encarga de inicializar los componentes de
+ * la interfaz para mostrar la lista de deportes cargados desde el archivo de recursos
+ * correspondiente (arrays.xml). Declara la interfaz {@link OnSportsSelected} para ser implementada
+ * por la Actividad contenedora a la que comunicar la puntuación asignada a cada deporte.
+ */
 public class SportsListFragment extends BaseFragment {
+    /**
+     * Nombre de a clase
+     */
     private final static String TAG = SportsListFragment.class.getSimpleName();
+    /**
+     * Etiqueta para indicar, en la instanciación del Fragmento, el identificador del objeto al que
+     * van a aplicarse los deportes y las puntuaciones escogidas en la ejecución de este Fragmento.
+     */
     public static final String BUNDLE_INSTANCE_OBJECT_ID = "BUNDLE_INSTANCE_OBJECT_ID";
+    /**
+     * Etiqueta para indicar, en la instanciación del Fragmento, la lista de deportes y puntuaciones
+     * ya establecidas en una ejecución anterior y almacenadas en la base de datos.
+     */
     public static final String BUNDLE_INSTANCE_SPORT_LIST = "BUNDLE_INSTANCE_SPORT_LIST";
+    /**
+     * Etiqueta para indicar, en la instanciación del Fragmento, la lista de votos para que no se
+     * pierdan en la edición, sólo en caso de que se trate de editar las puntuaciones de pistas de
+     * una instalación.
+     */
     public static final String BUNDLE_INSTANCE_VOTES_LIST = "BUNDLE_INSTANCE_VOTES_LIST";
 
+    /**
+     * Referencia a la lista de la interfaz donde se van a colocar los deportes
+     */
     @BindView(R.id.recycler_list)
     RecyclerView sportsRecyclerViewList;
+
+    /**
+     * Adaptador para manejar la colección de deportes y mostrar sus datos y su barra de puntuación
+     * en cada una de las celdas de la lista.
+     */
     private AddSportsAdapter mSportAdapter;
 
+    /**
+     * Constructor
+     */
     public SportsListFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Método de instanciación del Fragmento
+     *
+     * @param id         identificador del objeto al que se van a asociar estos deportes y puntuaciones
+     * @param sportsList lista de deportes y puntuaciones de una posible ejecución anterior
+     * @param votesList  lista de votos para cada deporte, en el caso de que los haya
+     * @return una nueva instancia de SportsListFragment
+     */
     public static SportsListFragment newInstance(@NonNull String id,
                                                  ArrayList<Sport> sportsList,
                                                  HashMap<String, Long> votesList) {
@@ -60,12 +103,25 @@ public class SportsListFragment extends BaseFragment {
         return fragment;
     }
 
+    /**
+     * Instancia el Adaptador usado en la lista de deportes. Este Fragmento no inicializa el
+     * Presentador porque no se rige bajo el patrón MVP.
+     *
+     * @param savedInstanceState estado del Fragmento guardado en una posible rotación de
+     *                           la pantalla, o null.
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        mSportAdapter = new AddSportsAdapter(null, Glide.with(this));
     }
 
+    /**
+     * Inicializa el contenido del menú de opciones de la esquina superior derecha de la pantalla
+     *
+     * @param menu menú de opciones donde se van a emplazar los elementos.
+     */
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.clear();
@@ -73,6 +129,15 @@ public class SportsListFragment extends BaseFragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
+    /**
+     * Invocado cuando un elemento del menú es pulsado. En este caso se encarga de aceptar y enviar
+     * las puntuaciones establecidas, y los votos si los hubiera, a la Actividad contenedora que
+     * debió implementar la interfaz {@link OnSportsSelected}
+     *
+     * @param item elemento del menú pulsado
+     * @return true si se aceptó la pulsación, false en otro caso
+     * @see #extractHashMapVotesFromBundle()
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
@@ -92,13 +157,50 @@ public class SportsListFragment extends BaseFragment {
         return false;
     }
 
+    /**
+     * Extrae, si los hubiera, el número de votos y el deporte al que van destinados del
+     * {@link Bundle} de argumentos especificado en la instanciación del Fragmento.
+     *
+     * @return {@link HashMap} con el identificador del deporte como clave y el número de votos
+     * como valor.
+     */
+    @SuppressWarnings("unchecked")
+    private HashMap<String, Long> extractHashMapVotesFromBundle() {
+        HashMap<String, Long> votesListFromActivity = null;
+        if (getArguments() != null && getArguments().containsKey(BUNDLE_INSTANCE_VOTES_LIST)) {
+            Serializable serializable = getArguments().getSerializable(BUNDLE_INSTANCE_VOTES_LIST);
+            if (serializable instanceof HashMap)
+                votesListFromActivity = (HashMap) serializable;
+        }
+        return votesListFromActivity;
+    }
+
+
+    /**
+     * Inicializa y obtiene una referencia a los elementos de la interfaz con la ayuda de
+     * ButterKnife. Establece {@link #mSportAdapter} como adaptador de la lista de la interfaz
+     * recién inflada.
+     * <p>
+     * Además, comprueba que la Actividad contenedora implementa la interfaz
+     * {@link OnSportsSelected} para comunicar el resultado de la ejecución de este Fragmento.
+     *
+     * @param inflater           utilizado para inflar el archivo de layout
+     * @param container          contenedor donde se va a incluir la interfaz o null
+     * @param savedInstanceState estado del Fragmento guardado en una posible rotación de
+     *                           la pantalla, o null.
+     * @return la vista de la interfaz inicializada
+     * @see <a href= "http://jakewharton.github.io/butterknife/">ButterKnife</a>
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_list, container, false);
         ButterKnife.bind(this, root);
 
-        mSportAdapter = new AddSportsAdapter(null, Glide.with(this));
+        if (!(getActivity() instanceof OnSportsSelected)) {
+            Log.e(TAG, "onCreateView: getActivity() should implement OnSportsSelected");
+            getActivity().onBackPressed();
+        }
         sportsRecyclerViewList.setAdapter(mSportAdapter);
         sportsRecyclerViewList.setHasFixedSize(true);
         sportsRecyclerViewList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
@@ -106,20 +208,41 @@ public class SportsListFragment extends BaseFragment {
         return root;
     }
 
-    private boolean isTheSameSport(Sport a, Sport b) {
-        return a.getSportID().equals(b.getSportID());
+    /**
+     * Al finalizar el proceso de creación de la Actividad contenedora, se invoca este método que
+     * establece un título para la barra superior y la acción que debe realizar: navegar hacia atrás.
+     *
+     * @param savedInstanceState estado del Fragmento guardado en una posible rotación de
+     *                           la pantalla, o null.
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mFragmentManagementListener.setCurrentDisplayedFragment(getString(R.string.pick_sports), this);
+        mNavigationDrawerManagementListener.setToolbarAsUp();
     }
 
+    /**
+     * Establece los deportes que debe mostrar la lista en el Adaptador, por medio de
+     * {@link #loadSports()}
+     */
     @Override
     public void onStart() {
         super.onStart();
         hideSoftKeyboard();
-        mFragmentManagementListener.setCurrentDisplayedFragment(getString(R.string.pick_sports), this);
-        if (mNavigationDrawerManagementListener != null) mNavigationDrawerManagementListener.setToolbarAsUp();
         mSportAdapter.replaceData(loadSports());
         showContent();
     }
 
+    /**
+     * Carga los identificadores de deportes desde el array declarado en el archivo de recursos
+     * arrays.xml, y crea para cada uno un {@link Sport} que será añadido al Adaptador de la lista.
+     * También especifica una puntuación de inicio si ese deporte ya la tenía de una ejecución
+     * anterior.
+     *
+     * @return lista de {@link Sport}s que serán añadidos al Adaptador de la lista para que los
+     * muestre en la interfaz
+     */
     private List<Sport> loadSports() {
         ArrayList<Sport> result = new ArrayList<>();
 
@@ -134,7 +257,7 @@ public class SportsListFragment extends BaseFragment {
             if (sportsListFromActivity != null) {
                 for (Sport sportFromActivity : sportsListFromActivity)
                     for (Sport sportFromResources : result)
-                        if (isTheSameSport(sportFromActivity, sportFromResources)) {
+                        if (sportFromActivity.getSportID().equals(sportFromResources.getSportID())) {
                             sportFromResources.setPunctuation(sportFromActivity.getPunctuation());
                             break;
                         }
@@ -144,23 +267,28 @@ public class SportsListFragment extends BaseFragment {
         return result;
     }
 
-    private HashMap<String, Long> extractHashMapVotesFromBundle() {
-        HashMap votesListFromActivity = null;
-        if (getArguments() != null && getArguments().containsKey(BUNDLE_INSTANCE_VOTES_LIST)) {
-            Serializable serializable = getArguments().getSerializable(BUNDLE_INSTANCE_VOTES_LIST);
-            if (serializable instanceof HashMap)
-                votesListFromActivity = (HashMap) serializable;
-        }
-        return votesListFromActivity;
-    }
-
+    /**
+     * Borra el contenido del Adaptador para que no se guarde en el estado del Fragmento.
+     */
     @Override
     public void onPause() {
         super.onPause();
         mSportAdapter.replaceData(null);
     }
 
+    /**
+     * Interfaz que debe ser implementada por la Actividad que contenga este Fragmento
+     * {@link SportsListFragment} para recibir la lista de deportes con la puntuación de cada uno.
+     */
     public interface OnSportsSelected {
+        /**
+         * Invocado para comunicar el resultado de la ejecución de este Fragmento
+         *
+         * @param id             identificador del objeto al que se van a asociar estos deportes y
+         *                       puntuaciones
+         * @param sportsSelected lista de deportes y puntuaciones resultado de la ejecución
+         * @param votesList      lista de votos para cada deporte, en el caso de que los haya
+         */
         void retrieveSportsSelected(String id,
                                     List<Sport> sportsSelected,
                                     HashMap<String, Long> votesList);
